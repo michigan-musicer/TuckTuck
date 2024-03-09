0e56f54ad00c6f18682bf5e50cf5085849f71a0b
michigan-musicer <musicer@umich.edu>
2024-03-09 22:23:28 +0100

    update logger to go to stderr

--- TuckTuck/src/Logger.cpp
+++ TuckTuck/src/Logger.cpp
@@ -1,22 +1,24 @@
 #include <Logger.h>
-
-#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace TuckTuck
 {
-    void Logger::init()
+    Logger::Logger()
     {
         // engineLogger = std::make_shared<spdlog::logger>("TUCKTUCK");
+        engineLogger = spdlog::stderr_color_mt("TUCKTUCK");
         // spdlog::register_logger(engineLogger);
-        // engineLogger->set_pattern("%H:%M:%S %z [thread %t] %v");
-        // engineLogger->set_level(spdlog::level::trace);
+        engineLogger->set_pattern("TUCKTUCK: %H:%M:%S %z [thread %t] %v");
+        engineLogger->set_level(spdlog::level::trace);
 
         // clientLogger = std::make_shared<spdlog::logger>("Client");
+        clientLogger = spdlog::stderr_color_mt("CLIENT");
         // spdlog::register_logger(clientLogger);
-        // // just including this as an example to myself that I can set different patterns
-        // // for different loggers
-        // clientLogger->set_pattern("%H:%M:%S %z [thread %t] [process %p] %v");
-        // clientLogger->set_level(spdlog::level::trace);
+
+        // just including this as an example to myself that I can set different patterns
+        // for different loggers
+        clientLogger->set_pattern("TUCKTUCKCLIENT: %H:%M:%S %z [thread %t] [process %p] %v");
+        clientLogger->set_level(spdlog::level::trace);
     }       
 }
 
